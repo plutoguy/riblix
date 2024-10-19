12a84f236d056ad7ce30d29690d941a1a7182fad
plutoguy <123932868+plutoguy@users.noreply.github.com>
2024-10-19 23:15:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://doctoraux.com';
+const nggUrl = 'https://educationbluesky.com';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
@@ -11,7 +11,7 @@ const proxy = createProxyMiddleware({
   secure: true,
   logLevel: 'debug',
   router: function(req) {
-    if (req.headers.host === 'doctoraux.com') {
+    if (req.headers.host === 'educationbluesky.com') {
       req.headers['X-Forwarded-For'] = ''; 
       req.headers['X-Real-IP'] = '';
       req.headers['Via'] = '';
